6e7bb3a148b084c76302e5105b65ceaaecb041d5
Jason Summers <jason1@pobox.com>
2013-04-22 21:18:04 -0400

    A hack to support (invalid?) OS/2v2 BMP files with 3 bytes per palette entry

--- src/imagew-bmp.c
+++ src/imagew-bmp.c
@@ -474,6 +474,19 @@ static int iwbmp_read_palette(struct iwbmprcontext *rctx)
 
 	b = (rctx->bmpversion==2) ? 3 : 4; // bytes per palette entry
 
+	if(rctx->infoheader_size==64) {
+		// According to what little documentation I can find, OS/2v2 BMP files
+		// have 4 bytes per palette entry. But some of the files I've seen have
+		// only 3. This is a little hack to support them.
+		if(rctx->fileheader_size + rctx->infoheader_size + rctx->palette_entries*3 ==
+			rctx->bfOffBits)
+		{
+			iw_warning(rctx->ctx,"BMP bitmap overlaps colormap; assuming colormap uses 3 bytes per entry instead of 4");
+			b = 3;
+			rctx->palette_nbytes = 3*rctx->palette_entries;
+		}
+	}
+
 	// If the palette has >256 colors, only use the first 256.
 	valid_palette_entries = (rctx->palette_entries<=256) ? rctx->palette_entries : 256;
 	valid_palette_nbytes = valid_palette_entries * b;
