78601eeafb9e2df693d3f4a18be820dd146c13dd
Jason Summers <jason1@pobox.com>
2015-10-18 17:06:21 -0400

    Fixed BMP decoding bug - Some BITFIELDS images weren't decoded correctly
    ... if some samples had exactly 8 bits, and others did not.

--- src/imagew-bmp.c
+++ src/imagew-bmp.c
@@ -923,14 +923,15 @@ static void iwbmpr_misc_config(struct iw_context *ctx, struct iwbmprcontext *rct
 
 	// Tell IW the significant bits.
 	if(rctx->bitcount==16 || rctx->bitcount==32) {
-		if(rctx->bf_bits_count[0]!=8)
+		if(rctx->bf_bits_count[0]!=8 || rctx->bf_bits_count[1]!=8 || rctx->bf_bits_count[2]!=8 ||
+			(IW_IMGTYPE_HAS_ALPHA(rctx->img->imgtype) && rctx->bf_bits_count[3]!=8))
+		{
 			iw_set_input_max_color_code(ctx,0, (1 << rctx->bf_bits_count[0])-1 );
-		if(rctx->bf_bits_count[1]!=8)
 			iw_set_input_max_color_code(ctx,1, (1 << rctx->bf_bits_count[1])-1 );
-		if(rctx->bf_bits_count[2]!=8)
 			iw_set_input_max_color_code(ctx,2, (1 << rctx->bf_bits_count[2])-1 );
-		if(IW_IMGTYPE_HAS_ALPHA(rctx->img->imgtype) && rctx->bf_bits_count[2]!=8) {
-			iw_set_input_max_color_code(ctx,3, (1 << rctx->bf_bits_count[3])-1 );
+			if(IW_IMGTYPE_HAS_ALPHA(rctx->img->imgtype)) {
+				iw_set_input_max_color_code(ctx,3, (1 << rctx->bf_bits_count[3])-1 );
+			}
 		}
 	}
 }
