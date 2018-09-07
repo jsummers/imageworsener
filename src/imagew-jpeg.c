f562fce4e9a561a0b3d0d0466b6b5221c3f76047
Jason Summers <jason1@pobox.com>
2018-09-07 11:48:22 -0400

    Reformatted code to reduce compiler warnings

--- src/imagew-jpeg.c
+++ src/imagew-jpeg.c
@@ -380,9 +380,12 @@ static void convert_cmyk_to_rbg(struct iw_context *ctx, const JSAMPLE *src,
 		r = 1.0 - c*(1.0-k) - k;
 		g = 1.0 - m*(1.0-k) - k;
 		b = 1.0 - y*(1.0-k) - k;
-		if(r<0.0) r=0.0; if(r>1.0) r=1.0;
-		if(g<0.0) g=0.0; if(g>1.0) g=1.0;
-		if(b<0.0) b=0.0; if(b>1.0) b=1.0;
+		if(r<0.0) r=0.0;
+		if(r>1.0) r=1.0;
+		if(g<0.0) g=0.0;
+		if(g>1.0) g=1.0;
+		if(b<0.0) b=0.0;
+		if(b>1.0) b=1.0;
 		dst[3*i+0] = (JSAMPLE)(0.5+255.0*r);
 		dst[3*i+1] = (JSAMPLE)(0.5+255.0*g);
 		dst[3*i+2] = (JSAMPLE)(0.5+255.0*b);
